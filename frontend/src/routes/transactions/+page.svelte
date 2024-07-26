7c62f958927b1d3e7ca826f16714ae51b3ae7de6
Timen van Gelderen <thvangelderen@gmail.com>
2024-07-26 16:47:00 +0200

    moved form dialog to new component

--- frontend/src/routes/transactions/+page.svelte
+++ frontend/src/routes/transactions/+page.svelte
@@ -1,11 +1,10 @@
 <script lang="ts">
     import Plus from 'lucide-svelte/icons/plus';
-    import X from 'lucide-svelte/icons/x';
     import { page } from '$app/stores';
-    import TransactionForm from '$lib/components/transactionForm.svelte';
     import TransactionsList from '$lib/components/transactionsList.svelte';
     import TransactionInfoModal from '$lib/components/transactionInfoModal.svelte';
     import type { Transaction } from '../../ambient';
+    import TransactionFormModal from '$lib/components/transactionFormModal.svelte';
 
     const { transactionIntervals, incomeTypes, expenseTypes } = $page.data;
 
@@ -21,7 +20,7 @@
         return months;
     }
 
-    let modal: HTMLDialogElement;
+    let showFormModal = $state(false);
     let month = $state(Number.parseInt(new Date().toLocaleString('default', { month: 'numeric' })));
     let transactions: Transaction[] = $state([]);
     let selectedTransaction: Transaction | null = $state(null);
@@ -32,14 +31,6 @@
 
     function selectTransaction(transaction: Transaction | null) {
         selectedTransaction = transaction;
-    }
-
-    function showModal() {
-        modal.showModal();
-    }
-
-    function closeModal() {
-        modal.close();
     }
 
     async function fetchTransactions() {
@@ -63,24 +54,24 @@
     <p>Track your finances with ease and gain valuable insights.</p>
 </div>
 
-<div class="grid sm:grid-cols-3 mb-10 lg:mb-16 rounded-3xl bg-primary-500/20 [&>div]:p-4 [&>div>span]:text-3xl">
-    <div class="flex flex-col justify-between">
+<div class="grid sm:grid-cols-3 mb-10 lg:mb-16 rounded-3xl bg-primary-500/20">
+    <div class="p-4 flex flex-col justify-between items-center sm:items-start">
         <h4 class="mb-6">Total income</h4>
-        <span>€{income}</span>
+        <span class="text-3xl">€{income}</span>
     </div>
-    <div class="flex flex-col justify-between border-t-[1px] border-b-[1px] sm:border-t-[0px] sm:border-b-[0px] sm:border-l-[1px] sm:border-r-[1px] border-primary-700/25">
+    <div class="p-4 flex flex-col justify-between items-center sm:items-start border-t-[1px] border-b-[1px] sm:border-t-[0px] sm:border-b-[0px] sm:border-l-[1px] sm:border-r-[1px] border-primary-700/25">
         <h4 class="mb-6">Total expense</h4>
-        <span>€{expense}</span>
+        <span class="text-3xl">€{expense}</span>
     </div>
-    <div class="flex flex-col justify-between">
+    <div class="p-4 flex flex-col justify-between items-center sm:items-start">
         <h4 class="mb-6">Net income</h4>
-        <span>€{netIncome}</span>
+        <span class="text-3xl">€{netIncome}</span>
     </div>
 </div>
 
 <div class="flex justify-between items-center flex-col sm:flex-row my-4">
     <h2>Transactions</h2>
-    <button class="btn secondary mt-4 sm:mt-0 " onclick={showModal}><Plus />&nbsp;Add transaction</button>
+    <button class="btn secondary mt-4 sm:mt-0 " onclick={() => showFormModal = true}><Plus />&nbsp;Add transaction</button>
 </div>
 <div class="card w-full p-4">
     <select id="month-selector" class="select" bind:value={month}>
@@ -91,9 +82,6 @@
     <TransactionsList {transactions} {selectTransaction} />
 </div>
 
-<dialog class="w-[500px] max-w-[95%]" bind:this={modal}>
-    <button class="absolute top-4 right-4" onclick={closeModal}><X /></button>
-    <TransactionForm {transactionIntervals} {incomeTypes} {expenseTypes} {closeModal} />
-</dialog>
+<TransactionFormModal {transactionIntervals} {incomeTypes} {expenseTypes} open={showFormModal} onclose={() => showFormModal = false} />
 
 <TransactionInfoModal transaction={selectedTransaction} onclose={() => selectTransaction(null)} />
