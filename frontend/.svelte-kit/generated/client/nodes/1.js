8cd5f4caa3c9fbdedf7b53ee554fe1ae6aa4ec2b
TvGelderen <thvangelderen@gmail.com>
2024-07-28 20:25:59 +0200

    Delete frontend/.svelte-kit directory

--- frontend/.svelte-kit/generated/client/nodes/1.js
+++
@@ -1,1 +0,0 @@
-export { default as component } from "../../../../node_modules/.pnpm/@sveltejs+kit@2.5.18_@sveltejs+vite-plugin-svelte@3.1.1_svelte@5.0.0-next.183_vite@5.3.3__sve_wr6oamlesaqxclqdwmlfetwsae/node_modules/@sveltejs/kit/src/runtime/components/error.svelte";
